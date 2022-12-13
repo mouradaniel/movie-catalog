87237836b5c27ef1d8e8269bdbb87d5744496315
Daniel Moura <daniel-moura@outlook.com>
2022-12-13 18:03:29 -0300

    feat: add movie list on app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Header } from "./components/Header";
+import { MovieList } from './components/MovieList';
 
 function App() {
   return (
     <div>
       <Header />
+      <MovieList />
     </div>
   );
 }
