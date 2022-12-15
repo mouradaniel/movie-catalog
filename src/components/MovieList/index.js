603a89fb1be6a84bb65c60fc9a45c89f260a96b1
Daniel Moura <daniel-moura@outlook.com>
2022-12-14 21:47:17 -0300

    feat: change to use context and dinamyc title list by context

--- src/components/MovieList/index.js
+++ src/components/MovieList/index.js
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useContext } from 'react';
+
+import SearchContext from '../../context/Search';
 
 import { MovieItem } from '../MovieItem';
 
@@ -10,33 +11,17 @@ import {
 } from './styles';
 
 export function MovieList() {
-  const [movies, setMovies] = useState([]);
+  const { listTitle, movieList } = useContext(SearchContext);
 
-  useEffect(() => {
-    try {
-      axios.get("https://api.themoviedb.org/3/movie/popular?api_key=28d5df611ec841cf9780851fb5c907f1")
-        .then(({ data }) => {
-          if (data.results) {
-            setMovies(data.results)
-          }
-        })
-        .catch((error) => {
-          console.log(error)
-        })      
-    } catch (error) {
-      console.log("Error on get a popular movies ", error)
-    }
-  }, [])
-
-  if (!movies.length) {
+  if (!movieList.length) {
     return <div>Loading movies...</div>
   }
 
   return (
     <Container>
-      <Title>Popular Movies</Title>
+      <Title>{listTitle}</Title>
       <Content>
-        {movies.map((movie) => {
+        {movieList.map((movie) => {
           return (
             <MovieItem key={movie.id} movie={movie} />
           )
