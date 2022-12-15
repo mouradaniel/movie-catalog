1ee89332477cf1a5f2b973ea1a28c2388849d869
Daniel Moura <daniel-moura@outlook.com>
2022-12-14 21:42:24 -0300

    feat: implement search context

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
+import { SearchProvider } from './context/Search';
+
 import { Header } from "./components/Header";
 import { GenreList } from "./components/GenreList";
 import { MovieList } from './components/MovieList';
 
 
 function App() {
+
   return (
-    <div>
-      <Header />
-      <GenreList />
-      <MovieList />
-    </div>
+    <SearchProvider>
+      <div>
+        <Header />
+        <GenreList />
+        <MovieList />
+      </div>
+    </SearchProvider>
   );
 }
 
---
+++ src/context/Search.js
@@ -0,0 +1,71 @@
+import React, { createContext, useState, useEffect } from "react";
+import axios from "axios";
+
+const SearchContext = createContext();
+
+const API_URL = "https://api.themoviedb.org/";
+const API_KEY = "api_key=28d5df611ec841cf9780851fb5c907f1"
+
+export const SearchProvider = ({ children }) => {
+  const [listTitle, setListTitle] = useState("");
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(() => {
+    try {
+      axios.get(`${API_URL}/3/movie/popular?${API_KEY}`)
+        .then(({ data }) => {
+          if (data.results) {
+            setListTitle("Popular Movies")
+            setMovieList(data.results)
+          }
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    } catch (error) {
+      console.log("Error on get a popular movies ", error)
+    }
+  }, []);
+
+  async function getMovieListByTerm(term) {
+    try {
+      axios.get(`${API_URL}/3/search/movie?${API_KEY}&query=${term}`)
+        .then(({ data }) => {
+          if (data.results) {
+            setListTitle(`Results for: ${term}`)
+            setMovieList(data.results)
+          }
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    } catch (error) {
+      console.log("Error on get movie list by term ", error)
+    }
+  }
+
+  async function getMovieListByGender(id, name) {
+    try {
+      axios.get(`${API_URL}/3/discover/movie?${API_KEY}&with_genres=${id}`)
+        .then(({ data }) => {
+          if (data.results) {
+            setListTitle(name)
+            setMovieList(data.results)
+          }
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    } catch (error) {
+      console.log("Error on get movie list by gender ", error)
+    }
+  }
+
+  return (
+    <SearchContext.Provider value={{API_URL, listTitle, movieList, getMovieListByTerm, getMovieListByGender}}>
+      {children}
+    </SearchContext.Provider>
+  );
+};
+
+export default SearchContext;
