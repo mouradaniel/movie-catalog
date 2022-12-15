bcd5b500c789d4cedc3f3cd09337a9e2b405ff29
Daniel Moura <daniel-moura@outlook.com>
2022-12-15 17:41:47 -0300

    feat: add link on movie items to go movie details page

--- src/components/MovieItem/index.js
+++ src/components/MovieItem/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import {
   Container,
   Title,
@@ -8,10 +10,12 @@ import {
 
 export function MovieItem({ movie }) {
   return (
-    <Container>
-      <Title title={movie.title}>{movie.title}</Title>
-      <Poster src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`} alt={movie.title} />
-      <Overview title={movie.overview}>{movie.overview}</Overview>      
-    </Container>
+    <Link to={`/movie/${movie.id}`}>
+      <Container>
+        <Title title={movie.title}>{movie.title}</Title>
+        <Poster src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`} alt={movie.title} />
+        <Overview title={movie.overview}>{movie.overview}</Overview>      
+      </Container>
+    </Link>
   )
 }
