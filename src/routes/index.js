30319bb8bcc50b94b12d4c5211b7c2bc56d40b5a
Daniel Moura <daniel-moura@outlook.com>
2022-12-15 17:45:18 -0300

    feat: implement movie details route

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 
 import { Header } from "../components/Header";
 import Home from "../pages/Home";
+import MovieDetails from "../pages/MovieDetails";
 
 export default function AppRoutes() {
   return (
@@ -9,6 +10,7 @@ export default function AppRoutes() {
       <Header />
       <Routes>
         <Route index element={<Home />} />
+        <Route path="/movie/:id" element={<MovieDetails />} />
       </Routes>
     </>
   )
