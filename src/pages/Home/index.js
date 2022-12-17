717c4f5369cd4d13160145e07ba5d6d7ceaf7eff
Daniel Moura <daniel-moura@outlook.com>
2022-12-17 11:05:45 -0300

    feat: change get popular movies on home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,25 @@
+import { useContext, useEffect } from 'react';
+
+import SearchContext from '../../context/Search';
+
 import { GenreList } from "../../components/GenreList";
 import { MovieList } from '../../components/MovieList';
 
+import { useTranslation } from "react-i18next";
+
 export default function Home() {
+  const { t } = useTranslation();
+
+  const { getPopularMovies, listTitle } = useContext(SearchContext);
+  
+  useEffect(() => {
+    getPopularMovies()
+  }, []);
+
   return (
     <>
       <GenreList />
-      <MovieList />
+      <MovieList page="home" title={listTitle} />
     </>
   );
 }
