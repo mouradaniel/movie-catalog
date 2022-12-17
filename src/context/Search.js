9976dda5695fff069b6800867be5f7616f88b2b5
Daniel Moura <daniel-moura@outlook.com>
2022-12-17 10:24:46 -0300

    feat: implement set language on api

--- src/context/Search.js
+++ src/context/Search.js
@@ -1,6 +1,8 @@
 import React, { createContext, useState, useEffect } from "react";
 import axios from "axios";
 
+import { useTranslation } from "react-i18next";
+
 const SearchContext = createContext();
 
 const API_URL = "https://api.themoviedb.org/";
@@ -9,13 +11,16 @@ const API_KEY = "api_key=28d5df611ec841cf9780851fb5c907f1"
 export const SearchProvider = ({ children }) => {
   const [listTitle, setListTitle] = useState("");
   const [movieList, setMovieList] = useState([]);
+  const [language, setLanguage] = useState("en");
+
+  const { t } = useTranslation();
 
   useEffect(() => {
     try {
-      axios.get(`${API_URL}/3/movie/popular?${API_KEY}`)
+      axios.get(`${API_URL}/3/movie/popular?${API_KEY}&language=${language}`)
         .then(({ data }) => {
           if (data.results) {
-            setListTitle("Popular Movies")
+            setListTitle(t('popular-title'))
             setMovieList(data.results)
           }
         })
@@ -25,11 +30,11 @@ export const SearchProvider = ({ children }) => {
     } catch (error) {
       console.log("Error on get a popular movies ", error)
     }
-  }, []);
+  }, [language]);
 
   async function getMovieListByTerm(term) {
     try {
-      axios.get(`${API_URL}/3/search/movie?${API_KEY}&query=${term}`)
+      axios.get(`${API_URL}/3/search/movie?${API_KEY}&query=${term}&language=${language}`)
         .then(({ data }) => {
           if (data.results) {
             setListTitle(`Results for: ${term}`)
@@ -46,7 +51,7 @@ export const SearchProvider = ({ children }) => {
 
   async function getMovieListByGender(id, name) {
     try {
-      axios.get(`${API_URL}/3/discover/movie?${API_KEY}&with_genres=${id}`)
+      axios.get(`${API_URL}/3/discover/movie?${API_KEY}&with_genres=${id}&language=${language}`)
         .then(({ data }) => {
           if (data.results) {
             setListTitle(name)
@@ -62,13 +67,23 @@ export const SearchProvider = ({ children }) => {
   }
 
   async function getMovieById(id) {
-    const response = await axios.get(`${API_URL}/3/movie/${id}?${API_KEY}`)
+    const response = await axios.get(`${API_URL}/3/movie/${id}?${API_KEY}&language=${language}`)
     return response.data;
   }
 
 
   return (
-    <SearchContext.Provider value={{API_URL, API_KEY, listTitle, movieList, getMovieListByTerm, getMovieListByGender, getMovieById}}>
+    <SearchContext.Provider value={{
+      API_URL, 
+      API_KEY, 
+      language, 
+      listTitle, 
+      movieList, 
+      getMovieListByTerm, 
+      getMovieListByGender, 
+      getMovieById,
+      setLanguage
+    }}>
       {children}
     </SearchContext.Provider>
   );
