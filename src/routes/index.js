b29c040abe2e261e6b07d561953469a77c2669f6
Daniel Moura <daniel-moura@outlook.com>
2022-12-17 10:24:02 -0300

    feat: import language bar

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 
 import { Header } from "../components/Header";
+import { LanguageBar } from "../components/LanguageBar";
 import Home from "../pages/Home";
 import MovieDetails from "../pages/MovieDetails";
 
 export default function AppRoutes() {
   return (
     <>
+      <LanguageBar />
       <Header />
       <Routes>
         <Route index element={<Home />} />
